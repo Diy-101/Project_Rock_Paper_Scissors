0e0f0221a5c941adf4def40aeefe94b3f19c0756
Diy-101 <thehorsediy101@gmail.com>
2024-03-03 23:38:01 +0300

    Add function "playround"

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,35 @@
 
 
 function getComputerChoice() {
-    let array = ["Rock", "Paper", "Scissors"];
+    let array = ["rock", "paper", "scissors"];
     let result = array[Math.floor(Math.random() * 3)];
     return result; 
 }
+
+
+function playRound(playerSelection, computerSelection) {
+    let player = playerSelection.toLowerCase()
+
+    if (computerSelection == player) return "Draw!";
+
+    else if (computerSelection == "rock") {
+        if (player == "paper") return "You WIN! Paper beats Rock";
+        else if (player == "scissors") return "You LOSE! Rock beats Scissors";
+    }
+
+    else if (computerSelection == "paper") {
+        if (player == "scissors") return "You WIN! Scissors beats Paper"
+        else if (player == "rock") return "You LOSE! Paper beats Rock"
+    }
+
+    else if (computerSelection == "scissors") {
+        if (player == "rock") return "You WIN! Rock beats Scissors"
+        else if (player == "paper") return "You LOSE! Scissors beats Paper"
+  }
+  return 
+}
+  
+  const playerSelection = "ROCK";
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
+  
